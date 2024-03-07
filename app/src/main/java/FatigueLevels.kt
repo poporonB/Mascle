e8594e94b2c05a0bd60a239556718d60a5d4ca2d
hi19miyazaki <hi19miyazaki@g.kumamoto-nct.ac.jp>
2024-03-07 11:00:58 +0900

    編集0307_11:00

---
+++ app/src/main/java/FatigueLevels.kt
@@ -0,0 +1,10 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mascle.R
+
+class FatigueLevels : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fatigurelevels)
+    }
+}
