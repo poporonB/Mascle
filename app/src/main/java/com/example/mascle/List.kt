a62cc84e44efbf451e290d53940aecf765b67279
yamas <yamashun.0504@gmail.com>
2024-03-10 13:52:01 +0900

    とりあえずcsvファイルを1行ずつ読み込み配列に格納

--- app/src/main/java/com/example/mascle/List.kt
+++ app/src/main/java/com/example/mascle/List.kt
@@ -4,17 +4,51 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mascle.R.id.start
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class List : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        val button = findViewById<Button>(R.id.start)
+        val button = findViewById<Button>(start)
         button.setOnClickListener {
             val intent = Intent(this, List::class.java)
             startActivity(intent)
             finish()
         }
+
+        val lines = readCSVFromAssets("trainingdata.csv") // sample.csv を実際のファイル名に変更する
+
+        // CSVファイルの各行を格納する配列
+        val csvData = ArrayList<String>()
+
+        // 読み込んだCSVファイルの各行を配列に格納
+        for (line in lines) {
+            csvData.add(line)
+        }
+
+        // 配列に格納されたCSVデータを使って
+        for (data in csvData) {
+            // dataに対する処理
+        }
+    }
+
+    private fun readCSVFromAssets(fileName: String): ArrayList<String> {
+        val resultList = ArrayList<String>()
+        try {
+            val inputStream = assets.open(fileName)
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                resultList.add(line!!)
+            }
+            inputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return resultList
     }
 }
